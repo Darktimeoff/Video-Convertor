c62e4cfae758006050f71834e40c69a9ed86af50
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-04-06 22:26:55 +0300

    add prompt service

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { PromptService } from "./core/prompt/prompt.service";
 export class App {
   async run() {
     const res = await new PromptService().input<number>("Number", "number");
-    console.log("res", res);
+    console.log("res", res, typeof res);
     return res;
   }
 }
