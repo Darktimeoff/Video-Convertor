294d60becd4c8fba66ac67ee1520fc8684ad30b8
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-04-06 22:54:47 +0300

    fix console-logger

---
+++ src/out/console-logger/console-logger.service.ts
@@ -0,0 +1,29 @@
+import { IStreamLogger } from "../../core/handlers/stream-logger.interface";
+
+export class ConsoleLogger implements IStreamLogger {
+  private static instance: ConsoleLogger | undefined;
+
+  // eslint-disable-next-line
+    private constructor() {};
+
+  getInstance() {
+    if (!ConsoleLogger.instance) {
+      // eslint-disable-next-line
+            ConsoleLogger.instance = new ConsoleLogger();
+    }
+
+    return ConsoleLogger.instance;
+  }
+
+  log(...args: any[]) {
+    console.log(...args);
+  }
+
+  error(...args: any[]): void {
+    console.error(args);
+  }
+
+  end(): void {
+    console.log("Готово");
+  }
+}
